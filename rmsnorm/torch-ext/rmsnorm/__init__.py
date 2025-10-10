30477d12a8075a1f28767c136632ffeb3f5c582a
Wang, Yi A <yi.a.wang@intel.com>
2025-10-10 11:02:47 +0000

    also expose apply_rms_norm
    Signed-off-by: Wang, Yi A <yi.a.wang@intel.com>

--- rmsnorm/torch-ext/rmsnorm/__init__.py
+++ rmsnorm/torch-ext/rmsnorm/__init__.py
@@ -1,4 +1,14 @@
 from . import layers
 
-__all__ = ["layers"]
+from ._ops import ops
+
+
+def apply_rms_norm(input, weight, eps):
+    return ops.apply_rms_norm(
+            input,
+            weight,
+            eps,
+    )
+
+__all__ = ["layers", "apply_rms_norm"]
 
