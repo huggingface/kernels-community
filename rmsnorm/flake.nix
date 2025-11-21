c54a6bb37e2e9c1f6cd5cb8e386835b5323828c6
Daniël de Kok <me@danieldk.eu>
2025-11-21 10:20:48 +0000

    Only build on x86_64-linux for now

--- rmsnorm/flake.nix
+++ rmsnorm/flake.nix
@@ -8,5 +8,14 @@
     kernel-builder.lib.genFlakeOutputs {
       inherit self;
       path = ./.;
+
+      # This is a workaround, we should be able to specify flags per arch in
+      # kernel-builder.
+      torchVersions =
+        allVersions:
+        builtins.map (
+          version:
+          version // { systems = builtins.filter (system: system == "x86_64-linux") version.systems; }
+        ) allVersions;
     };
 }
