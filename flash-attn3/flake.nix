0b000c33a85f8e9bb54dea71007b5d7b8e74a6ad
Daniël de Kok <me@danieldk.eu>
2025-11-24 08:39:04 +0000

    flash-attn3: fixes + build for CUDA >= 12.8
    - Set project name `flash_attn3` -> `flash-attn3`.
    - Sync compiler flags with upstream.
    - Build on CUDA => 12.8. 12.6 still fails with an internal compiler
      error, but 12.8 is the default for Torch now anyway + this will enable
      CUDA 13.

--- flash-attn3/flake.nix
+++ flash-attn3/flake.nix
@@ -13,23 +13,5 @@
     kernel-builder.lib.genFlakeOutputs {
       inherit self;
       path = ./.;
-      # Building with CUDA later than 12.4 fails with:
-      #
-      # error: 'ptxas' died due to signal 11 (Invalid memory reference)
-      #
-      # So, build for 12.4 only and copy to all the other build variants
-      # by hand (which works fine thanks to backward compat).
-      torchVersions = _: [
-        {
-          torchVersion = "2.9";
-          cudaVersion = "12.4";
-          cxx11Abi = true;
-          systems = [
-            "x86_64-linux"
-            "aarch64-linux"
-          ];
-          bundleBuild = true;
-        }
-      ];
     };
 }
