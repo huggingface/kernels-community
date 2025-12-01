72d7059e202cbe924a22cf3a6df2de059e7c46c7
jiqing-feng <jiqing.feng@intel.com>
2025-12-01 16:31:55 +0000

    convert to bfloat16 in custom op
    Signed-off-by: jiqing-feng <jiqing.feng@intel.com>

--- quantization-bitsandbytes/torch-ext/quantization_bitsandbytes/custom_ops.py
+++ quantization-bitsandbytes/torch-ext/quantization_bitsandbytes/custom_ops.py
@@ -8,4 +8,12 @@ def gemm_4bit_forward(
     blocksize: int,
     quant_type: int,
 ) -> torch.Tensor:
-    return ops.gemm_4bit_forward(input, weight, absmax, blocksize, quant_type)
+    original_dtype = input.dtype
+    if original_dtype != torch.bfloat16:
+        input = input.to(torch.bfloat16)
+
+    output = ops.gemm_4bit_forward(input, weight, absmax, blocksize, quant_type)
+    if original_dtype != torch.bfloat16:
+        output = output.to(original_dtype)
+
+    return output
