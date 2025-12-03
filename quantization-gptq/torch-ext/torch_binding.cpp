cca45514981902d7b0191e70e5f7584906ef2ac8
jiqing-feng <jiqing.feng@intel.com>
2025-12-03 15:23:19 +0000

    fix torch binding
    Signed-off-by: jiqing-feng <jiqing.feng@intel.com>

--- quantization-gptq/torch-ext/torch_binding.cpp
+++ quantization-gptq/torch-ext/torch_binding.cpp
@@ -6,24 +6,25 @@
 torch::Tensor gemm_int4_cpu_forward(
     const torch::Tensor &input,
     const torch::Tensor &weight,
+    const torch::Tensor &zeros,
     const torch::Tensor &absmax,
-    int64_t blocksize,
-    int64_t quant_type);
+    int64_t blocksize);
 #endif
 // Unified dispatcher for both CPU and XPU
 torch::Tensor gemm_int4_forward(
     const torch::Tensor &input,
     const torch::Tensor &weight,
+    const torch::Tensor &zeros,
     const torch::Tensor &absmax,
-    int64_t blocksize,
-    int64_t quant_type) {
+    int64_t blocksize) {
 #if defined(CPU_KERNEL)
     if (input.device().type() == torch::kCPU) {
         TORCH_CHECK(input.device().type() == torch::kCPU, "input must be on CPU");
         TORCH_CHECK(weight.device().type() == torch::kCPU, "weight must be on CPU");
+        TORCH_CHECK(zeros.device().type() == torch::kCPU, "zeros must be on CPU");
         TORCH_CHECK(absmax.device().type() == torch::kCPU, "absmax must be on CPU");
         TORCH_CHECK(blocksize > 0, "blocksize must be > 0");
-        return gemm_int4_cpu_forward(input, weight, absmax, blocksize, quant_type);
+        return gemm_int4_cpu_forward(input, weight, zeros, absmax, blocksize);
     }
 #endif
     else {
@@ -32,7 +33,7 @@ torch::Tensor gemm_int4_forward(
 }
 
 TORCH_LIBRARY_EXPAND(TORCH_EXTENSION_NAME, ops) {
-  ops.def("gemm_int4_forward(Tensor input, Tensor weight, Tensor absmax, int blocksize, int quant_type) -> Tensor");
+  ops.def("gemm_int4_forward(Tensor input, Tensor weight, Tensor zeros, Tensor absmax, int blocksize) -> Tensor");
 #if defined(CPU_KERNEL)
   // Register CPU implementation
   ops.impl("gemm_int4_forward", torch::kCPU, &gemm_int4_forward);
