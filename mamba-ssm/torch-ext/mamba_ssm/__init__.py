ea443b644919f9e5caba792e67d67792c27e4258
medmekk <mohamed.mekkouri@huggingface.co>
2025-12-11 09:26:31 +0000

    fix

--- mamba-ssm/torch-ext/mamba_ssm/__init__.py
+++ mamba-ssm/torch-ext/mamba_ssm/__init__.py
@@ -1,6 +1,7 @@
 __version__ = "2.2.4"
 
 from .ops import selective_state_update, mamba_chunk_scan_combined, mamba_split_conv1d_scan_combined
+from .ops.selective_scan_interface import selective_scan_fn, mamba_inner_fn
 from .modules import Mamba, Mamba2
 from .models import MambaLMHeadModel
 
