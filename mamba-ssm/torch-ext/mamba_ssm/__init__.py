4d430b75daab6ea7f04d709107c62d1ae0bbaa06
medmekk <mohamed.mekkouri@huggingface.co>
2025-12-11 13:40:10 +0000

    update for falcon

--- mamba-ssm/torch-ext/mamba_ssm/__init__.py
+++ mamba-ssm/torch-ext/mamba_ssm/__init__.py
@@ -5,7 +5,8 @@ from .ops.selective_scan_interface import selective_scan_fn, mamba_inner_fn
 from .modules import Mamba, Mamba2
 from .models import MambaLMHeadModel
 
+falcon_mamba_inner_fn = mamba_inner_fn
 __all__ = [
-    "selective_scan_fn", "mamba_inner_fn", "selective_state_update", "mamba_chunk_scan_combined", "mamba_split_conv1d_scan_combined",
+    "selective_scan_fn", "mamba_inner_fn", "falcon_mamba_inner_fn", "selective_state_update", "mamba_chunk_scan_combined", "mamba_split_conv1d_scan_combined",
     "Mamba", "Mamba2", "MambaLMHeadModel",
 ]
