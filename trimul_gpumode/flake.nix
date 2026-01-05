49d12d830775f994230b7fb77057e10c928b9d4a
Daniël de Kok <me@danieldk.eu>
2026-01-05 16:13:46 +0100

    trimul_gpumode: build for Torch 2.10 RC3 (#147)
    * trimul_gpumode: build for Torch 2.10 RC3
    
    * Metal and CPU are not supported

--- trimul_gpumode/flake.nix
+++ trimul_gpumode/flake.nix
@@ -2,7 +2,7 @@
   description = "Flake for trimul_gpumode kernels";
 
   inputs = {
-    kernel-builder.url = "github:huggingface/kernel-builder";
+    kernel-builder.url = "github:huggingface/kernel-builder/torch-2.10";
   };
 
   outputs =
