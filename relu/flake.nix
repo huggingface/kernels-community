9f1f069aaebf6888a0b4e8e2da58025c62ab5568
Daniël de Kok <me@danieldk.eu>
2026-01-06 14:40:40 +0100

    relu: build for Torch 2.10 RC3 (#141)
    * relu: build for Torch 2.10 RC3
    
    * CI: use macOS 26 for macOS builds
    
    * Fix Mac runner version
    
    * Install Metal toolchain

--- relu/flake.nix
+++ relu/flake.nix
@@ -2,7 +2,7 @@
   description = "Flake for ReLU kernel";
 
   inputs = {
-    kernel-builder.url = "github:huggingface/kernel-builder";
+    kernel-builder.url = "github:huggingface/kernel-builder/torch-2.10";
   };
 
   outputs =
