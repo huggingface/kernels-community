43e21443cc6182dbc4b1f4be16cd15ddef37dece
Daniël de Kok <me@danieldk.eu>
2026-01-07 14:45:34 +0000

    megablocks: build for Torch 2.10 RC4

--- megablocks/flake.nix
+++ megablocks/flake.nix
@@ -2,7 +2,7 @@
   description = "Flake for megablocks_moe kernel";
 
   inputs = {
-    kernel-builder.url = "github:huggingface/kernel-builder/torch-2.10";
+    kernel-builder.url = "github:huggingface/kernel-builder";
   };
 
   outputs =
