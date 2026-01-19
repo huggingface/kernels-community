1e10d74ddad54fe577165a273cbe530482565d1b
Daniël de Kok <me@danieldk.eu>
2026-01-19 12:21:33 +0100

    punica-sgmv: build for Torch 2.10 RC7 (#255)

--- punica-sgmv/flake.nix
+++ punica-sgmv/flake.nix
@@ -2,7 +2,7 @@
   description = "Flake for Punica SGMV kernel";
 
   inputs = {
-    kernel-builder.url = "github:huggingface/kernel-builder/version-option";
+    kernel-builder.url = "github:huggingface/kernel-builder";
   };
 
   outputs =
