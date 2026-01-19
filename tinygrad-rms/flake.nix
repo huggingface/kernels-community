eb79f2bf95fb3dd1ec76f886d7aaa663197ea147
Daniël de Kok <me@danieldk.eu>
2026-01-19 12:18:13 +0100

    tinygrad-rms: build for Torch 2.10 RC7 (#264)

--- tinygrad-rms/flake.nix
+++ tinygrad-rms/flake.nix
@@ -2,7 +2,7 @@
   description = "Flake for tinygrad-style RMSNorm kernel";
 
   inputs = {
-    kernel-builder.url = "github:huggingface/kernel-builder/version-option";
+    kernel-builder.url = "github:huggingface/kernel-builder";
   };
 
   outputs =
