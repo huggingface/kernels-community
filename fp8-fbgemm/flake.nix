3443d9d77745ab357c4f9b47be45038c04c7421c
Daniël de Kok <me@danieldk.eu>
2026-01-19 12:17:58 +0100

    fp8-fbgemm: build for Torch 2.10 RC7 (#248)

--- fp8-fbgemm/flake.nix
+++ fp8-fbgemm/flake.nix
@@ -2,7 +2,7 @@
   description = "Flake for fp8-fbgemm kernels";
 
   inputs = {
-    kernel-builder.url = "github:huggingface/kernel-builder/version-option";
+    kernel-builder.url = "github:huggingface/kernel-builder";
   };
 
   outputs =
