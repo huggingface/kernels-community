b4b07d008cda4a35c071a7643785df30aa8b9b4e
Daniël de Kok <me@danieldk.eu>
2026-01-19 10:44:40 +0000

    scattermoe: build for Torch 2.10 RC7

--- scattermoe/flake.nix
+++ scattermoe/flake.nix
@@ -2,7 +2,7 @@
   description = "Flake for scattermoe kernels";
 
   inputs = {
-    kernel-builder.url = "github:huggingface/kernel-builder/version-option";
+    kernel-builder.url = "github:huggingface/kernel-builder";
   };
 
   outputs =
