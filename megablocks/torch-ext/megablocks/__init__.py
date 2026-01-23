eb0f2c25607ed0be50be841b5f1d791b5e924bd8
jiqing-feng <jiqing.feng@intel.com>
2026-01-23 16:31:35 +0800

    revert init
    Signed-off-by: jiqing-feng <jiqing.feng@intel.com>

--- megablocks/torch-ext/megablocks/__init__.py
+++ megablocks/torch-ext/megablocks/__init__.py
@@ -17,17 +17,6 @@ from ._layers.moe import MoE, ParallelMLP, get_load_balancing_loss
 
 from . import layers
 
-# Import device-specific fused MoE implementations
-from . import cpu_fused_moe
-from . import xpu_fused_moe
-
-# Import C++ optimized version
-try:
-    from . import cpu_moe_cpp
-    HAS_CPP_MOE = True
-except (ImportError, RuntimeError):
-    HAS_CPP_MOE = False
-
 # This section contains the direct kernel exports (not inlcuded in the original code)
 def exclusive_cumsum(x: torch.Tensor, dim: int, out: torch.Tensor) -> torch.Tensor:
     """
