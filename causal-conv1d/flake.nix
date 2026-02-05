b75a0f919e3a6894dd783a65ec0587877d95470f
Daniël de Kok <me@danieldk.eu>
2026-02-05 09:49:30 +0000

    causal-conv1d: test build2cmake refactor

--- causal-conv1d/flake.nix
+++ causal-conv1d/flake.nix
@@ -2,7 +2,7 @@
   description = "Flake for attention kernels";
 
   inputs = {
-    kernel-builder.url = "github:huggingface/kernels";
+    kernel-builder.url = "github:huggingface/kernels/remove-backend";
   };
 
   outputs =
