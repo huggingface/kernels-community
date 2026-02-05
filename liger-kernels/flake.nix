875ab8bdb2e0da01a84bd3a41a31da88cbe22c3b
Daniël de Kok <me@danieldk.eu>
2026-02-05 09:50:02 +0000

    liger-kernels: test build2cmake refactor

--- liger-kernels/flake.nix
+++ liger-kernels/flake.nix
@@ -2,7 +2,7 @@
   description = "Flake for Unsloth Kernels";
 
   inputs = {
-    kernel-builder.url = "github:huggingface/kernels";
+    kernel-builder.url = "github:huggingface/kernels/remove-backend";
   };
 
   outputs =
