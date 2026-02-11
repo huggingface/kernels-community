270c50fad3e08f2a545e7fbb00ea04dc02d846cc
Daniël de Kok <me@danieldk.eu>
2026-02-11 14:48:56 +0000

    relu: add and test layer

--- relu/tests/test_relu.py
+++ relu/tests/test_relu.py
@@ -16,4 +16,17 @@ def test_relu():
     else:
         device = torch.device("cpu")
     x = torch.randn(1024, 1024, dtype=torch.float32, device=device)
-    torch.testing.assert_allclose(F.relu(x), relu.relu(x))
+    torch.testing.assert_close(F.relu(x), relu.relu(x))
+
+def test_relu_layer():
+    if platform.system() == "Darwin":
+        device = torch.device("mps")
+    elif hasattr(torch, "xpu") and torch.xpu.is_available():
+        device = torch.device("xpu")
+    elif torch.version.cuda is not None and torch.cuda.is_available():
+        device = torch.device("cuda")
+    else:
+        device = torch.device("cpu")
+    x = torch.randn(1024, 1024, dtype=torch.float32, device=device)
+    layer = relu.layers.ReLU()
+    torch.testing.assert_close(F.relu(x), layer(x))
