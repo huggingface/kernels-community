46a6040d50620aa43820bf62899df367f3cabbba
David Holtz <david.richard.holtz@gmail.com>
2026-02-20 16:27:28 -0500

    fix: prefer remote builder

--- deep-gemm/flake.nix
+++ deep-gemm/flake.nix
@@ -2,8 +2,7 @@
   description = "Flake for DeepGEMM kernel";
 
   inputs = {
-    # kernel-builder.url = "github:huggingface/kernels";
-    kernel-builder.url = "path:/home/drbh/Projects/kernels";
+    kernel-builder.url = "github:huggingface/kernels/support-bundling-deps";
   };
 
   outputs =
