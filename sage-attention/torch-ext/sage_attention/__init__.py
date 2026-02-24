f3937843f98f8984c978e685fd3a99207df94434
medmekk <mohamed.mekkouri@huggingface.co>
2026-02-24 11:14:40 +0000

    upstream

--- sage-attention/torch-ext/sage_attention/__init__.py
+++ sage-attention/torch-ext/sage_attention/__init__.py
@@ -1,6 +1,11 @@
 from .quant import per_block_int8, per_warp_int8, sub_mean, per_channel_fp8
 from .core import sageattn
 
+try:
+    from .sm100_compile import sageattn3_blackwell
+    SM100_ENABLED = True
+except Exception:
+    SM100_ENABLED = False
 
 __all__ = [
     "per_block_int8",
@@ -8,4 +13,5 @@ __all__ = [
     "sub_mean",
     "per_channel_fp8",
     "sageattn",
+    "sageattn3_blackwell",
 ]
