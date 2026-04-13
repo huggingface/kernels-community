d678caa824df5b6a1902a26abc0dc832fd81e99e
Daniël de Kok <me@danieldk.eu>
2026-04-13 13:29:04 +0000

    sonic-moe: build only on x86_64-linux for now

--- sonic-moe/flake.nix
+++ sonic-moe/flake.nix
@@ -13,5 +13,17 @@
     kernel-builder.lib.genKernelFlakeOutputs {
       inherit self;
       path = ./.;
+
+      # aarch64 get-kernel fails because it cannot load libcuda.so when
+      # loading cutlass. We have to look into this, but we can filter out
+      # aarch64 in the meanwhile. Since this is a noarch kernel, the
+      # resulting build will work on aarch64 as well.
+      torchVersions =
+        allVersions:
+        builtins.map (
+          version:
+          version // { systems = builtins.filter (system: system == "x86_64-linux") version.systems; }
+        ) allVersions;
+
     };
 }
