58475d75de91aa8fa5b2e512e6d6e7c11afb9d34
Daniël de Kok <me@danieldk.eu>
2026-05-07 08:31:19 +0000

    Update flake to pick up backend-specific CI output

--- relu/flake.nix
+++ relu/flake.nix
@@ -2,7 +2,7 @@
   description = "Flake for ReLU kernel";
 
   inputs = {
-    kernel-builder.url = "github:huggingface/kernels/torch-2.12";
+    kernel-builder.url = "github:huggingface/kernels";
   };
 
   outputs =
