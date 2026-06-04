a617dfc02b50be73d729ea14c3cbb4c104840f17
vasqu <antonprogamer@gmail.com>
2026-06-04 21:30:48 +0200

    add fn to init

--- liger-kernels/torch-ext/liger_kernels/__init__.py
+++ liger-kernels/torch-ext/liger_kernels/__init__.py
@@ -1,8 +1,9 @@
 from . import layers
-from .layers import LigerForCausalLMLoss
+from .layers import LigerForCausalLMLoss, liger_rotary_pos_emb
 
 
 __all__ = [
     "layers",
     "LigerForCausalLMLoss",
+    "liger_rotary_pos_emb",
 ]
