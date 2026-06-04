92d71faa9ca049ecc7138b98e3dd84899833874e
vasqu <antonprogamer@gmail.com>
2026-06-04 16:36:18 +0200

    functions are only init

--- rotary/torch-ext/rotary/__init__.py
+++ rotary/torch-ext/rotary/__init__.py
@@ -1,5 +1,5 @@
-from . import ext, layers
+from .ext import apply_rotary_transformers
 from .layers import apply_rotary
 
 
-__all__ = ["apply_rotary", "ext", "layers"]
+__all__ = ["apply_rotary", "apply_rotary_transformers"]
