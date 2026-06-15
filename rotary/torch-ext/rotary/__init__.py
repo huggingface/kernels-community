7bcd1a4fec6d583e9353034a2b5a3cf8c61c1af0
vasqu <antonprogamer@gmail.com>
2026-06-15 17:45:05 +0200

    oops

--- rotary/torch-ext/rotary/__init__.py
+++ rotary/torch-ext/rotary/__init__.py
@@ -1,7 +1,6 @@
-from . import layers
 from .layers import apply_rotary_transformers
 from .rotary import apply_rotary
 
 
 # Keeping `apply_rotary` for BC
-__all__ = ["apply_rotary", "apply_rotary_transformers", "layers"]
+__all__ = ["apply_rotary", "apply_rotary_transformers"]
