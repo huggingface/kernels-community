2a8dac0d95097b30f469337f4a97dbe7fa7aa1b7
Daniël de Kok <me@danieldk.eu>
2026-06-17 12:18:22 +0200

    Move flakes to main branch and auto-update build.toml (#957)
    * Move flakes to main branch and auto-update build.toml
    
    - Go back the `kernels` main branch for kernel builder.
    - Update the flake lock file.
    - Auto-update `build.toml` to the latest version.
    
    * nix fmt

--- rotary/flake.nix
+++ rotary/flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Flake for Torch kernel extension";
   inputs = {
-    kernel-builder.url = "github:huggingface/kernels/torch-2.12";
+    kernel-builder.url = "github:huggingface/kernels";
   };
   outputs =
     { self, kernel-builder }:
@@ -9,14 +9,14 @@
       inherit self;
       path = ./.;
 
-    torchVersions =
-      let
-        # For CPU builds, only x86_64-linux is currently supported.
-        cpuSupported = version: system: !(version ? "cpu") || system == "x86_64-linux";
-      in
-      allVersions:
-      builtins.map (
-        version: version // { systems = builtins.filter (cpuSupported version) version.systems; }
-      ) allVersions;
+      torchVersions =
+        let
+          # For CPU builds, only x86_64-linux is currently supported.
+          cpuSupported = version: system: !(version ? "cpu") || system == "x86_64-linux";
+        in
+        allVersions:
+        builtins.map (
+          version: version // { systems = builtins.filter (cpuSupported version) version.systems; }
+        ) allVersions;
     };
 }
