092ae2b60786617c410587a161de1cbf632e7e9b
Daniël de Kok <me@danieldk.eu>
2026-07-01 11:50:37 +0200

    vllm-flash-attn3: switch to Torch stable ABI (#998)
    * vllm-flash-attn3: port to Torch stable ABI
    
    * vllm-flash-attn3: avoid duplicate compilation
    
    * vllm-flash-attn3: add very mimimal build configuration for testing
    
    * vllm-flash-attn3: use ptxas 12.8 for CUDA 12.6
    
    * vllm-flash-attn3: use `get_kernel` for tests
    
    * vllm-flash-attn3: add missing test files
    
    * nix fmt
    
    * Add more info at the top of `build.toml.test` on what it disables
    
    * Clarify `torchVersions` modification

--- vllm-flash-attn3/flake.nix
+++ vllm-flash-attn3/flake.nix
@@ -2,7 +2,7 @@
   description = "Flake for Hopper Flash Attention kernel";
 
   inputs = {
-    kernel-builder.url = "github:huggingface/kernels";
+    kernel-builder.url = "github:huggingface/kernels/framework-cxx-flags";
   };
 
   outputs =
@@ -13,5 +13,13 @@
     kernel-builder.lib.genKernelFlakeOutputs {
       inherit self;
       path = ./.;
+
+      # Use ptxas from CUDA 12.8 for CUDA 12.6, since ptxas 12.6 crashes when building FA3.
+      torchVersions =
+        allVersions:
+        builtins.map (
+          version:
+          if (version.cudaVersion or null) == "12.6" then version // { ptxasVersion = "12.8"; } else version
+        ) allVersions;
     };
 }
