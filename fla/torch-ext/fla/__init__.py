afc214d376cf4c5f79d64cbe1387a3beb16eda90
vasqu <antonprogamer@gmail.com>
2026-07-29 22:43:54 +0200

    gdn

--- fla/torch-ext/fla/__init__.py
+++ fla/torch-ext/fla/__init__.py
@@ -1,10 +1,12 @@
 from . import layers
+from .ops.gated_delta_rule import chunk_gated_delta_rule, fused_recurrent_gated_delta_rule
 from .ops.kda import chunk_kda, fused_recurrent_kda
 
 
 __all__ = [
     # original functions directly
     "chunk_kda", "fused_recurrent_kda",
+    "chunk_gated_delta_rule", "fused_recurrent_gated_delta_rule",
     # layers
     "layers",
 ]
